805d9d692dc6a9801fb6d913ff2c148c18721423
shruti <shrutimahalkar@gmail.com>
2019-11-13 11:33:37 +0530

    shrut,,,,,

--- src/main/java/com/springsecurity/demo/RunApplication.java
+++ src/main/java/com/springsecurity/demo/RunApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RunApplication {
 	public static void main(String[] args) {
+		System.out.println("shruti");
         SpringApplication.run(RunApplication.class, args);
     }
 }
